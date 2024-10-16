adb6d27f213d7b1c1a4d0a6c8f4eb844a0bc7471
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-10-17 02:36:28 +0300

    create basic GUI without intersction, day 16

---
+++ venv/gui.py
@@ -0,0 +1,11 @@
+import FreeSimpleGUI as sg
+from bonus_samples import functions
+
+label = sg.Text("type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My TO_Do_App', layout=[[label, input_box, add_button]])
+window.read()
+
+window.close()
