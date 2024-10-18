104d58649829e1ad204d5d77e52c2a0fee134b32
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-10-18 08:49:31 +0300

    we implimented the Edith button

--- gui.py
+++ gui.py
@@ -4,23 +4,33 @@ import functions
 label = sg.Text("type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-
-window = sg.Window('My TO_Do_App', layout=[[label, input_box, add_button]],
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+window = sg.Window('My TO_Do_App', layout=[[label, input_box, add_button], [list_box, edit_button ]],
                    font=('Helvetica', 20))
 
 while True:
-    event, value = window.read()
+    event, values = window.read()
     print(event)
-    print(value)
+    print(values)
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = value['todo'] + "\n"
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
         case sg.WIN_CLOSED:
             break
 
+        case "Edit":
+            todo_to_edit = values[todos][0]
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case sg.WIN_CLOSED:
+            break
 
 
 window.close()
